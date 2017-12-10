e2041adbbad700f0eb110d30bde69875ab2cf2bd
Carlos Kazuo <ninetails.nine@gmail.com>
2017-12-10 12:59:21 -0200

    Reset to a previous state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react'
 
 import Toggle from './components/Toggle'
-import MyToggle from './components/MyToggle'
-import MyEventComponent from './components/MyEventComponent'
-
-const MyToggleWrapped = Toggle.withToggle(MyToggle)
-
-const MyEventComponentWrapped = Toggle.withToggle(MyEventComponent)
 
 class App extends Component {
   render() {
     return (
-      <Toggle onToggle={on => on ? this.myToggle.focus() : undefined}>
-        <Toggle.On>The button is on</Toggle.On>
-        <Toggle.Off>The button is off</Toggle.Off>
-        <Toggle.Button/>
-        <hr/>
-        <MyToggleWrapped innerRef={myToggle => this.myToggle = myToggle} />
-        <hr/>
-        <MyToggleWrapped.ToggleMessage/>
-      </Toggle>
+      <Toggle
+        onToggle={on => console.log('toggle', on)}
+        />
     );
   }
 }
--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -1,63 +1,14 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-import hoistNonReactStatic from 'hoist-non-react-statics';
 import Switch from '../Switch'
 
-export const TOGGLE_CONTEXT = '__toggle__'
-
-const withToggle = (Component) => {
-  function Wrapper({ innerRef, ...props }, context) {
-    const toggleContext = context[TOGGLE_CONTEXT]
-    return <Component toggle={toggleContext} ref={innerRef} {...props} />
-  }
-
-  Wrapper.contextTypes = {
-    [TOGGLE_CONTEXT]: PropTypes.object.isRequired
-  }
-
-  Wrapper.displayName = `withToggle(${Component.displayName || Component.name})`
-
-  return hoistNonReactStatic(Wrapper, Component)
-}
-
-const ToggleOn = ({ children, toggle: { on } }) =>
-  on ? children : null
-
-const ToggleOff = ({ children, toggle: { on } }) =>
-  on ? null : children
-
-const ToggleButton = ({ toggle: { on, toggle }, ...props }) =>
-  <Switch id="switch" on={on} onChange={toggle} {...props} />
-
 export default class Toggle extends Component {
-  static On = withToggle(ToggleOn)
-
-  static Off = withToggle(ToggleOff)
-
-  static Button = withToggle(ToggleButton)
-
-  static withToggle = withToggle
-
-  static defaultProps = { onToggle: () => undefined };
-
-  static childContextTypes = {
-    [TOGGLE_CONTEXT]: PropTypes.object.isRequired
-  }
-
   state = { on: false }
 
   toggle = () => this.setState(({ on }) => ({ on: !on }), () => this.props.onToggle(this.state.on))
 
-  getChildContext() {
-    return {
-      [TOGGLE_CONTEXT]: {
-        on: this.state.on,
-        toggle: this.toggle
-      }
-    }
-  }
-
   render() {
-    return this.props.children
+    const { on } = this.state
+    return <Switch id="switch" on={on} onChange={this.toggle} />
   }
 }
