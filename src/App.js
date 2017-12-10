e6e5b7a66208ab20f769b67ccbb82335f1c6f529
Carlos Kazuo <ninetails.nine@gmail.com>
2017-12-10 11:26:02 -0200

    Done lesson 02

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Toggle from './components/Toggle'
 class App extends Component {
   render() {
     return (
-      <Toggle onToggle={on => console.log('toggle', on)} />
+      <Toggle onToggle={on => console.log('toggle', on)}>
+        <Toggle.On>The button is on</Toggle.On>
+        <Toggle.Off>The button is off</Toggle.Off>
+        <Toggle.Button/>
+      </Toggle>
     );
   }
 }
--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -1,15 +1,40 @@
-import React, { Component } from 'react'
+import React, { cloneElement, Children, Component } from 'react'
 import Switch from '../Switch'
+
+export function ToggleOn({ on, children }) {
+  return on ? children : null;
+}
+
+export function ToggleOff({ on, children }) {
+  return on ? null : children;
+}
+
+export function ToggleButton({ on, toggle, ...props }) {
+  return <Switch id="switch" on={on} onChange={toggle} {...props} />
+}
 
 export default class Toggle extends Component {
   static defaultProps = { onToggle: () => undefined };
+
+  static On = ToggleOn
+
+  static Off = ToggleOff
+
+  static Button = ToggleButton
 
   state = { on: false }
 
   toggle = () => this.setState(({ on }) => ({ on: !on }), () => this.props.onToggle(this.state.on))
 
   render() {
-    const { on } = this.state
-    return (<Switch id="switch" on={on} onChange={this.toggle} />)
+    const children = Children.map(
+      this.props.children,
+      child => cloneElement(child, {
+        on: this.state.on,
+        toggle: this.toggle
+      })
+    )
+
+    return children
   }
 }
