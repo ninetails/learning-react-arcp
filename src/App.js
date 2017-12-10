20bd8a3b8af765ffc84b8d9799dba5e417fd637a
Carlos Kazuo <ninetails.nine@gmail.com>
2017-12-10 14:27:55 -0200

    Added MyInputs example

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react'
 
-import Toggle from './components/Toggle'
-import Switch from './components/Switch'
+import MyInputs from './components/MyInputs'
 
 const App = () => (
-  <Toggle
-    onToggle={on => console.log('toggle', on)}
-    onReset={on => console.log('reset', on)}
-    render={({ on, toggle, reset, getTogglerProps }) => [
-      <Switch key='switch' id="switch" on={on} {...getTogglerProps()} />,
-      <hr key='hr' />,
-      <button key="button" onClick={() => reset()}>Reset</button>
-    ]}
-    />
+  <MyInputs />
 )
 
 export default App;
